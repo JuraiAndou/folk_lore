8808ddc4a6e9a2a891f9be1b1581cc9e89416228
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2022-07-14 18:02:38 -0300

    Diálogo Saci Final

--- main.gd
+++ main.gd
@@ -2,7 +2,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var dialog = Dialogic.start("dia" + str(Mundo.dia) +"-"+ str(Mundo.suspAtual.getName()))#Dialogic.start("dia2-Saci")
+	var dialog = Dialogic.start("dia2-Saci")#Dialogic.start("dia" + str(Mundo.dia) +"-"+ str(Mundo.suspAtual.getName()))
 	#print("dia" + str(Mundo.dia) + str(Mundo.suspAtual.getName()))
 	add_child(dialog)
 
